dfd422b7c410cc0ee467317802f630308f71b589
KaneJons <strelnikov7953@gmail.com>
2022-04-13 22:34:36 +0300

    first commit

---
+++ ПЗ_14/DataTier/Преподаватель.cs
@@ -0,0 +1,12 @@
+﻿namespace DataTier
+{
+    public class Преподаватель
+    {
+        public String ФИО { get; set; }
+        public String Группа { get; set; }
+        public int Курс { get; set; }
+        public int Задолжность { get; set; }
+
+
+    }
+}
---
+++ ПЗ_14/LogicTier/Колледж.cs
@@ -0,0 +1,56 @@
+﻿using DataTier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTier
+{
+    public class Колледж
+    {
+        private List<ПозицияПреподавателей> _преподаватель = new List<ПозицияПреподавателей>();
+        
+        
+        public Колледж()
+        {
+            List<Преподаватель> tmp = ВсеПреподаватели.ПолучитьВсеТовары();
+            foreach (var t in tmp)
+            {
+                _преподаватель .Add(new ПозицияПреподавателей(t));
+            }
+        }
+        public List<ПозицияПреподавателей> СписокПреподаватель
+        {
+            get { return _преподаватель; }
+        }
+        public String НаименованиеКолледжа
+        {
+            get { return "РКСИ"; }
+        }
+        public string Всегозадолжностейпокурсам
+        {
+            get
+            {
+                string s = "Курс ";
+                
+                for (int i = 1;i <=_преподаватель.Max(p=>p.КурсПреподаватель);i++)
+                    switch(i== _преподаватель.Max(p => p.КурсПреподаватель))
+                    {
+                        case true: s += $"{i}:{_преподаватель.Where(p => p.КурсПреподаватель == i).Sum(p => p.ЗадолжностьПреподаватель)}."; break;
+                        default:
+                            s += $"{i}:{_преподаватель.Where(p => p.КурсПреподаватель == i).Sum(p => p.ЗадолжностьПреподаватель)}; "; break;
+                    }
+                
+                return s;
+            }
+        }
+        public int СтудентыбезЗадолжности
+        {
+            get
+            {
+                return _преподаватель.Count(p =>p.ЗадолжностьПреподаватель==0);
+            }
+        }
+    }
+}
